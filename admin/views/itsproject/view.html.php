deff77d201dc3dd03dbb72eeace5aa46eaae48f3
Brad Hancock <kidtangerine@gmail.com>
2016-11-29 10:59:14 -0600

    version .9 bugs

--- admin/views/itsproject/view.html.php
+++ admin/views/itsproject/view.html.php
@@ -51,6 +51,9 @@ class ITSProjectViewITSProject extends JViewLegacy
  
 		// Display the template
 		parent::display($tpl);
+		
+		// Set the document
+		$this->setDocument();
 	}
  
 	/**
@@ -85,4 +88,19 @@ class ITSProjectViewITSProject extends JViewLegacy
 			$isNew ? 'JTOOLBAR_CANCEL' : 'JTOOLBAR_CLOSE'
 		);
 	}
+	
+	/**
+	 * Method to set up the document properties
+	 *
+	 * @return void
+	 */
+	protected function setDocument() 
+	{
+		$isNew = ($this->item->id < 1);
+		$document = JFactory::getDocument();
+		$document->setTitle($isNew ? JText::_('COM_ITSPROJECT_ITSPROJECT_CREATING') :
+                JText::_('COM_ITSPROJECT_ITSPROJECT_EDITING'));
+	}
+	
+	
 }
